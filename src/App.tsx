c06453bbee2567eade74497fb16ed5745b862434
Abhishek Kumar <skylake1302@gmail.com>
2021-09-20 21:40:55 +0200

    Added AppErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Header } from "./components";
 import { store } from './state/store';
+import { AppErrorBoundary } from "./ErrorBoundry";
 
 const MovieSearchView = lazy(() => import('./views/MovieSearch'));
 const MovieDetailView = lazy(() => import('./views/MovieDetailView'));
@@ -16,31 +17,32 @@ const theme = {
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-      <Router>
-        <ThemeProvider theme={theme}>
-        <Header>
-          <h3>Movie App</h3>
-        </Header>
-        <main>
-          <Switch>
-          <Route path='/:id'>
-              <Suspense fallback={<div>Loading...</div>}>
-                <MovieDetailView />
-              </Suspense>
-            </Route>
-            <Route path='/' exact>
-              <Suspense fallback={<div>Loading...</div>}>
-                <MovieSearchView />
-              </Suspense>
-            </Route>
-          </Switch>
-        </main>
-        </ThemeProvider>
-      </Router>
-      </Provider>
-      
+    <div>
+      <AppErrorBoundary>
+        <Provider store={store}>
+          <Router>
+            <ThemeProvider theme={theme}>
+              <Header>
+                <h3>Movie App</h3>
+              </Header>
+              <main>
+                <Switch>
+                  <Route path='/:id'>
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <MovieDetailView />
+                    </Suspense>
+                  </Route>
+                  <Route path='/' exact>
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <MovieSearchView />
+                    </Suspense>
+                  </Route>
+                </Switch>
+              </main>
+            </ThemeProvider>
+          </Router>
+        </Provider>
+      </AppErrorBoundary>
     </div>
   );
 }
